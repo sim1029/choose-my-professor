e5f718f4390e623c3031c9cb0a8e7311553251a1
sim1029 <sps67@pitt.edu>
2023-03-13 11:35:43 -0400

    add rmp logo as placeholder

--- cmp-client/src/App.tsx
+++ cmp-client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import rmpLogo from './assets/rmp-logo.png'
 import './App.css'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src={rmpLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
