b1fa8ac2f6f871d6595f5cb7cb22e97a0da650c4
sim1029 <sps67@pitt.edu>
2023-03-20 14:33:08 -0400

    finish initial professor results cards

--- cmp-client/src/components/CardHeaders.tsx
+++ cmp-client/src/components/CardHeaders.tsx
@@ -6,7 +6,7 @@ const CardHeaders = () => {
   return (
     <Grid item xs={12} mb={3}>
       <Grid container px={5}>
-        <CardHeader size={2}>Rating</CardHeader>
+        <CardHeader size={1}>Rating</CardHeader>
         <CardHeader size={4}>Name</CardHeader>
         <CardHeader size={4}>Tags</CardHeader>
         <CardHeader size={2}>Confidence</CardHeader>
--- cmp-client/src/components/ProfessorCard.tsx
+++ cmp-client/src/components/ProfessorCard.tsx
@@ -4,6 +4,8 @@ import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
+import LanguageIcon from "@mui/icons-material/Language";
+import internet from "../assets/internet.png";
 
 interface ProfessorCardProps {
   isTopCard?: boolean;
@@ -25,12 +27,14 @@ const ProfessorCard = ({ isTopCard }: ProfessorCardProps) => {
                 border: 1.5,
                 borderBottom: 0,
                 p: 1,
+                borderColor: "primary.main",
               }
             : {
                 color: "primary.dark",
                 border: 1.5,
                 borderBottom: 0,
                 p: 1,
+                borderColor: "primary.main",
               }
         }
       >
@@ -51,25 +55,83 @@ const ProfessorCard = ({ isTopCard }: ProfessorCardProps) => {
                   color: "secondary.main",
                 }
           }
+          alignItems="center"
         >
-          <Grid item xs={2}>
-            <Typography variant={"h4"}>6.9</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant={"body1"}>Simon Schueller</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant={"body2"}>Tag1, Tag2, Tag3...</Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography variant={"h6"}>100 %</Typography>
-          </Grid>
-          {/* <Stack direction="row" justifyContent="center">
-            <Typography variant={"caption"}>
-              This is a summary of the professor's data produced by the model. 
-              Or some other content is going to go here haven't decided yet
+          <Grid item xs={1}>
+            <Typography variant={"h6"} fontWeight="bold">
+              6.9
             </Typography>
-          </Stack> */}
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant={"body1"} fontWeight="bold">
+              Simon Schueller
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant={"body1"} fontWeight="bold">
+              Tag1, Tag2, Tag3...
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography variant={"body1"} fontWeight="bold">
+              100 %
+            </Typography>
+          </Grid>
+          <Grid item xs={1} container justifyContent="end">
+            <a href="https://www.ratemyprofessors.com/">
+              <img src={internet} width={32}></img>
+            </a>
+          </Grid>
+        </Grid>
+        <Divider></Divider>
+        <Grid
+          container
+          p={4}
+          color="secondary"
+          sx={{
+            bgcolor: "primary.main",
+            color: "secondary.main",
+          }}
+          alignItems="center"
+        >
+          <Grid item xs={5}>
+            <Stack direction="row" spacing={4}>
+              <Typography
+                variant={"subtitle1"}
+                fontWeight="bold"
+                flexShrink={0}
+              >
+                Info-1
+              </Typography>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ maxWidth: 300 }}
+                color="primary.dark"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid item xs={7}>
+            <Stack direction="row" spacing={4}>
+              <Typography
+                variant={"subtitle1"}
+                flexShrink={0}
+                fontWeight="bold"
+              >
+                Info-2
+              </Typography>
+              <Typography
+                variant={"subtitle2"}
+                sx={{ maxWidth: 300 }}
+                color="primary.dark"
+              >
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.
+              </Typography>
+            </Stack>
+          </Grid>
         </Grid>
       </Paper>
     </Grid>
